55535e44be29733c98a26d3374647b150b365a92
HexaYang <983688828@qq.com>
2016-04-30 13:22:24 +0800

    add data for test

--- test.php
+++ test.php
@@ -1,3 +1,18 @@
 <?php 
-	
+	//1.将timestamp,nonce,token按字典序排序
+		$timestamp = $_GET['timestamp'];
+		$nonce     = $_GET['nonce'];
+		$token     = 'weixin';
+		$signature = $_GET['signature'];
+		$array     = array( $timestamp,$nonce,$token );
+		sort( $array );
+
+	//2.将排序后的三个参数拼接之后用aha1加密 
+		$tmpstr = implode('', $array);
+		$tmpstr = sha1( $tmpstr )
+	//3.将加密后的字符串与signature进行对比，判断该请求是否来自微信
+		if ( $tmpstr == $signature ) {
+			echo $_GET['eshostr'];
+			exit;
+		}
  ?>
